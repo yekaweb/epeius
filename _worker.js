0f248e9f00e7369979ff32ab5ccfe1b44ba4878a
yekaweb <rezaandroid0914@gmail.com>
2025-11-29 11:34:22 -0500

    cleanip_worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 
 import { connect } from "cloudflare:sockets";
 
-let password = '';
+let password = '12';
 let proxyIP = '';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
